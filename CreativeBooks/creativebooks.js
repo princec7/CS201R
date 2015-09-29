ad175b5656e7470865c4b2bd7c0d263993e67c43
Carl Prince <carlwprince@gmail.com>
2015-09-29 00:40:18 +0000

    Creative Books site added. Restattempt updated

---
+++ CreativeBooks/creativebooks.js
@@ -0,0 +1,37 @@
+$("#button").click(function(e){
+  var value = $("#isbn").val();
+  value = value.replace(/-/g,""); //Allows input to have hyphens (This line strips away hyphens)
+  console.log(value);
+  e.preventDefault();
+  var myurl= "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+  myurl += value;
+  console.log(myurl);
+  $.ajax({
+    url : myurl,
+    dataType : "jsonp",
+    success : function(parsed_json) {
+      console.log(parsed_json);
+      try {
+        $("#title").html(JSON.stringify(parsed_json['items'][0]['volumeInfo']['title']));
+        $("#author").html(JSON.stringify(parsed_json['items'][0]['volumeInfo']['authors'][0]));
+        $("#description").html(JSON.stringify(parsed_json['items'][0]['volumeInfo']['description']));
+      }
+      catch(e) { //If the ISBN entered does not return valid json, this block is run
+        $("#title").html('Invalid ISBN');
+        $("#author").html('');
+        $("#description").html('');
+      }
+    }
+  });
+});
+
+$('#isbn').one('click',(function(e){
+    $('#isbn').html('');
+}));
+
+$('#isbn').keypress(function(e) {
+    if (e.which == 13) {
+        $(document).ready(function() { $('#isbn').blur() });
+        $("#button").click();
+    }
+});
